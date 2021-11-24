bfcc3bde4c9c5c428ec2f9f7ae9b5bc969617f36
Max Leonard <maxhleonard@gmail.com>
2021-11-24 14:07:50 -0800

    for server

--- collect_data.py
+++ collect_data.py
@@ -4,7 +4,7 @@ import datetime
 import schedule
 import time
 
-QUERY = '''query { assets {name symbol statistic{liquidity shortValue volume apr{long short} marketCap collateralValue minCollateralRatio}}}'''
+QUERY = '''query { assets {name symbol prices{price, oraclePrice} statistic{liquidity shortValue volume apr{long short} marketCap collateralValue minCollateralRatio}}}'''
 BASE_URL = "https://graph.mirror.finance/graphql"
 
 def req_data():
@@ -13,7 +13,7 @@ def req_data():
 	data_df = pd.DataFrame()
 	data = requests.post(BASE_URL, json={"query":QUERY}).json()["data"]["assets"]
 	for asset in data:
-		data_df = data_df.append({"name":asset["name"], "symbol":asset["symbol"],"timestamp":now, "liquidity":asset["statistic"]["liquidity"], "shortValue":asset["statistic"]["shortValue"], "volume":asset["statistic"]["volume"], "apr_long":asset["statistic"]["apr"]["long"], "apr_short":asset["statistic"]["apr"]["short"], "marketCap":asset["statistic"]["marketCap"], "collateralValue":asset["statistic"]["collateralValue"], "minCollateralRatio":asset["statistic"]["minCollateralRatio"]}, ignore_index=True)
+		data_df = data_df.append({"name":asset["name"], "symbol":asset["symbol"],"timestamp":now,"price":asset["prices"]["price"], "oraclePrice":asset["prices"]["oraclePrice"], "liquidity":asset["statistic"]["liquidity"], "shortValue":asset["statistic"]["shortValue"], "volume":asset["statistic"]["volume"], "apr_long":asset["statistic"]["apr"]["long"], "apr_short":asset["statistic"]["apr"]["short"], "marketCap":asset["statistic"]["marketCap"], "collateralValue":asset["statistic"]["collateralValue"], "minCollateralRatio":asset["statistic"]["minCollateralRatio"]}, ignore_index=True)
 	data_df.to_csv("mirror_data.csv", header=False, index=False, mode="a")
 	print(data_df)
 
